33291eab2eff2ec816183ab0f927009830855139
031udaybkumar <udaybkumar031@OUTLOOK.com>
2016-07-29 14:37:41 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+class A
+{
+  public static void main(String[] args)
+  {
+     Scanner scn=new Scanner(System.in);
+     int t=scn.nextInt();
+     System.out.println(t);
+     int m[t][t]={1,1,3,6,8,1,2,2,5,5,4,4,0,3,3,8,0,2,3,4,4,3,6,2,1};
+     for(int[][] n:m)
+      {
+          
+     
+    }
+   }
